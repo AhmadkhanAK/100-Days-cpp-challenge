0dc4ad0260c3b83409482240c9d939b9fab0557e
Ahmed khan <114338100+AhmadkhanAK@users.noreply.github.com>
2023-02-07 20:33:37 +0500

    Add files via upload

---
+++ day30.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+string  test(string str)
+        {
+             if (str.length() < 3)
+            {
+                return str + str + str;
+            }
+            else
+            {
+                string front = str.substr(0, 3);
+                return front + str + front;
+            }
+        }
+        
+int main() 
+ {
+  cout << test("Python") << endl;  
+  cout << test("JS") << endl;  
+  cout << test("Code") << endl;  
+  return 0;    
+}
