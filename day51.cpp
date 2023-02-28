70a6a64dee81edc5dd64c3bba83d74ec1a3d49e9
Ahmed khan <114338100+AhmadkhanAK@users.noreply.github.com>
2023-02-28 19:26:14 +0500

    Add files via upload

---
+++ day51.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() 
+{ 
+	int a = 32, *ptr = &a; 
+	char ch = 'A', &cho = ch; 
+
+	cho += a; 
+	*ptr += ch; 
+	cout << a << ", " << ch << endl; 
+	return 0; 
+} 
