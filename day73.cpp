9fb610316ec1ee88a3070c5880ebe687112a9a4f
Ahmed khan <114338100+AhmadkhanAK@users.noreply.github.com>
2023-03-29 16:00:46 +0500

    Add files via upload

---
+++ day73.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int dividend, divisor, quotient;
+
+	try {
+		cout << "Enter the dividend: ";
+		cin >> dividend;
+		cout << endl;
+
+		cout << "Enter the divisor: ";
+		cin >> divisor;
+		cout << endl;
+
+		if (divisor == 0)
+			throw 0;
+
+		quotient = dividend / divisor;
+		cout << "Quotient: " << quotient << endl;
+	}
+	catch (int) {
+		cout << "Division by 0 is not allowed" << endl;
+	}
+	return 0;
+}
