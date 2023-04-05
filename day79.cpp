408ef99a5e6b41184839a971b2b61e01816d7e1e
Ahmed khan <114338100+AhmadkhanAK@users.noreply.github.com>
2023-04-05 22:00:04 +0500

    Create day79.cpp

---
+++ day79.cpp
@@ -0,0 +1,16 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main(){ int a[] = { 1,2,3,4,5 };
+  int t = a[4],i;
+  for(i = 4; i>=1; i--)
+  {
+      a[i]=a[i-1];
+  }
+  a[0]=t;
+  for(i = 0; i<=4; i++)
+  {
+    cout << a[i] << "\n";
+  }
+  return 0;
+}
